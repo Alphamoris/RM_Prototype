5e5bfe4414ded03565ada3978a057a0e81d61a06
Dhanush Kumar S <alphamoris45@gmail.com>
2025-02-23 09:56:40 +0530

    Update Editor.tsx

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useRef } from 'react';
 import EditorJS, { BlockToolConstructable, OutputData } from '@editorjs/editorjs';
-
 import Header from '@editorjs/header';
 import List from '@editorjs/list';
 import Code from '@editorjs/code';
